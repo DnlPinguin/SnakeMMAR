5d0985d45190ed3eff5e13937fa5ed3ede91e2c9
Daniel_Valentin <dkowalsk@students.uni-mainz.de>
2019-04-29 22:08:37 +0200

    label

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
 
+
+class Window(QtWidgets):
+
+    app = QtGui.QApplication(sys.argv)
+    w = QtGui.QWidget()
+    b = QtGui.QLabel(w)
+    b.setText("Hello World!")
+    w.setGeometry(100, 100, 200, 50)
+    b.move(50, 20)
+    w.setWindowTitle("PyQt2")
+    w.show()
+    sys.exit(app.exec_())
+
+
+
+if __name__ == "__main__":
+    Window()
