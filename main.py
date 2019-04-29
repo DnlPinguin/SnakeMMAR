92c4fef5a514a43b7526166907d276eeef534350
Daniel_Valentin <dkowalsk@students.uni-mainz.de>
2019-04-29 22:16:03 +0200

    label

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import random
 import sys
-from PyQt5 import QtWidgets
-from PyQt5 import QtGui
 
 
-class Window(QtWidgets):
+class SnakeGame(QMainWindow):
+    def __init__(self):
+        super(SnakeGame, self).__init__()
 
-    app = QtGui.QApplication(sys.argv)
-    w = QtGui.QWidget()
-    b = QtGui.QLabel(w)
-    b.setText("Hello World!")
-    w.setGeometry(100, 100, 200, 50)
-    b.move(50, 20)
-    w.setWindowTitle("PyQt2")
-    w.show()
+
+        #self.setCentralWidget(self.sboard)
+        self.setWindowTitle('PyQt5 Snake game')
+        self.resize(600, 400)
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+
+        self.show()
+
+
+def main():
+    app = QApplication([])
+    launch_game = SnakeGame()
     sys.exit(app.exec_())
 
 
-
-if __name__ == "__main__":
-    Window()
+if __name__ == '__main__':
+    main()
